f1f40b24705f5e60024be870e9e5fbf18fead718
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-10-09 21:15:05 +0100

    Add parsing

--- schema/user.py
+++ schema/user.py
@@ -5,6 +5,13 @@ class UserFieldEntry:
     value: str
     alt: str
 
+    @classmethod
+    def parse(cls, data: dict):
+        return cls(
+            value=data["value"],
+            alt=data["alt"],
+        )
+
 @dataclass(frozen=True)
 class UserAvatar:
     image_192: str
@@ -13,6 +20,41 @@ class UserAvatar:
     image_48: str
     image_512: str
     image_72: str
+
+    @classmethod
+    def parse(cls, data: dict):
+        return cls(
+            image_192=data["image_192"],
+            image_24=data["image_24"],
+            image_32=data["image_32"],
+            image_48=data["image_48"],
+            image_512=data["image_512"],
+            image_72=data["image_72"],
+        )
+
+@dataclass(frozen=True)
+class UserFlag:
+    is_admin: bool
+    is_app_user: bool
+    is_bot: bool
+    is_email_confirmed: bool
+    is_owner: bool
+    is_primary_owner: bool
+    is_restricted: bool
+    is_ultra_restricted: bool
+
+    @classmethod
+    def parse(cls, data: dict):
+        return cls(
+            is_admin=data["is_admin"],
+            is_app_user=data["is_app_user"],
+            is_bot=data["is_bot"],
+            is_email_confirmed=data["is_email_confirmed"],
+            is_owner=data["is_owner"],
+            is_primary_owner=data["is_primary_owner"],
+            is_restricted=data["is_restricted"],
+            is_ultra_restricted=data["is_ultra_restricted"],
+        )
 
 
 @dataclass(frozen=True)
@@ -31,6 +73,26 @@ class UserProfile:
     real_name_normalized: str
     skype: str
 
+    @classmethod
+    def parse(cls, data: dict):
+        return cls(
+            avatar_hash=data["avatar_hash"],
+            display_name=data["display_name"],
+            display_name_normalized=data["display_name_normalized"],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            fields={
+                k: UserFieldEntry.parse(v) for k, v in data["fields"].items()
+            },
+            avatars=UserAvatar.parse(data),
+            pronouns=data["pronouns"],
+            phone=data["phone"],
+            team=data["team"],
+            real_name=data["real_name"],
+            real_name_normalized=data["real_name_normalized"],
+            skype=data["skype"],
+        )
+
 @dataclass(frozen=True)
 class User:
     id: str
@@ -42,4 +104,20 @@ class User:
     tz_offset: int
     updated: int
     profile: UserProfile
+    flags: UserFlag
+
+    @classmethod
+    def parse(cls, data: dict):
+        return cls(
+            id=data["id"],
+            deleted=data["deleted"],
+            real_name=data["real_name"],
+            name=data["name"],
+            tz=data["tz"],
+            tz_label=data["tz_label"],
+            tz_offset=data["tz_offset"],
+            updated=data["updated"],
+            profile=UserProfile.parse(data["profile"]),
+            flags=UserFlag.parse(data)
+        )
     
