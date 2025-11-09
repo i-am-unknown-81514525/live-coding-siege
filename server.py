fabac59b9ec8c1774583d9da3d00a7e44996409d
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-09 22:16:33 +0000

    Added reject endpoint

--- server.py
+++ server.py
@@ -137,6 +137,19 @@ async def accept_turn(user_id: typing.Annotated[str, Depends(check_jwt)]):
     await get_result(db.update_turn_status, game_id, turn_user_id, "COMPLETED")
     return "ok"
 
+@app.get("/reject")
+async def reject_turn(user_id: typing.Annotated[str, Depends(check_jwt)]):
+    game_id = await get_result(db.get_game_mgr_active_game, user_id)
+    if game_id is None:
+        raise HTTPException(404, "Cannot find a game that you are actively manging")
+    turn_detail = await get_result(db.get_active_turn_details, game_id)
+
+    if not turn_detail:
+        raise HTTPException(419, "Unable to fine an active turn")
+
+    turn_user_id: str = turn_detail[0]
+
+    await get_result(db.update_turn_status, game_id, turn_user_id, "FAILED")
 
 @app.get("/turn-status")
 async def get_turn_status(user_id: typing.Annotated[str, Depends(check_jwt)]):
