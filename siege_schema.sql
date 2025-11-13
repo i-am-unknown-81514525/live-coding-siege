f9625ff0f3607dc682a4ce61a8e419f11bf9cde8
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-13 21:06:31 +0000

    Add schema for siege projects

---
+++ siege_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS game_record(
+    proj_id INT not null,
+    measuremen_time DATETIME not null DEFAULT CURRENT_TIMESTAMP,
+    week_num INT not null,
+    title TEXT not null,
+    description TEXT not null,
+    user_id INT not null,
+    hours FLOAT not null,
+    repo_url TEXT null,
+    demo_url TEXT null,
+    proj_status TEXT NOT NULL,
+    PRIMARY KEY (proj_id, measuremen_time)
+);
+
+CREATE TABLE IF NOT EXISTS user_record(
+    user_id INT not null,
+    measurement_time DATETIME not null DEFAULT CURRENT_TIMESTAMP,
+    coin_count INT not null,
+    user_status TEXT not null,
+);
