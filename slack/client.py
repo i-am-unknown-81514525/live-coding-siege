2d585d94c9de2dabd64baee7352fd7f915e580bd
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2025-11-29 19:29:17 +0000

    FIx typehint again why PyCharm and Pylance conflict on this lmao

--- slack/client.py
+++ slack/client.py
@@ -24,18 +24,18 @@ def process_message(client: "SlackClient", req: SocketModeRequest):
         event_type = event_payload.get("type")
 
         if event_type == "message" and "bot_id" not in event_payload:
-            event: MessageEvent = MessageEvent.parse(req.payload)
+            event = MessageEvent.parse(req.payload)
             message_dispatch(event, client)
 
         elif event_type == "user_huddle_changed":
-            event: HuddleChange = HuddleChange.parse(req.payload)
+            event = HuddleChange.parse(req.payload)
             huddle_dispatch(event, client)
 
     elif req.type == "interactive" and req.payload.get("type") == "block_actions":
-        event: BlockActionEvent = BlockActionEvent.parse(req.payload)
+        event = BlockActionEvent.parse(req.payload)
         action_dispatch(event, client)
     elif req.type == "slash_commands":
-        event: CommandEvent = CommandEvent.parse(req.payload)
+        event = CommandEvent.parse(req.payload)
         ctx = SlashContext(event, client)
         slash_dispatch(ctx)
 
